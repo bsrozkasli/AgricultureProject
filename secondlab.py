db7ac727b83b60987597f6a8e4500f620363a1bd
basar ozkasli <basarozkasli@gmail.com>
2024-09-12 14:21:32 +0300

    .\3791007.csv added

--- secondlab.py
+++ secondlab.py
@@ -1,2 +1,15 @@
-import numpy
-import pandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as  plt
+weather_data=pd.read_csv('/content/3791007.csv', parse_dates=['DATE'])
+weather_data['DATE'] = pd.to_datetime(weather_data['DATE'])
+weather_data.set_index('DATE', inplace=True)
+
+# Display the data
+weather_data.head()
+
+
+
+
+
+
